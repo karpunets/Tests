c3e72b214a7642c8f2d4ff4f4e6bb2403c02acd4
Viktor <Viktor.Kliui@gmail.com>
2017-11-16 17:27:30 +0200

    test find files1

--- Data/Make_requests_and_answers.py
+++ Data/Make_requests_and_answers.py
@@ -1,4 +1,4 @@
-import json, re, pytest, random, string, inspect,os
+import json, re, pytest, random, string, inspect,os, sys
 
 
 def random_string():
@@ -13,7 +13,7 @@ def make_test_data(json_name, data= {}, default=False):
     # curframe = inspect.currentframe()
     # calframe = inspect.getouterframes(curframe)
     # print(calframe[1][1])
-
+    print(sys.path)
     # Определяем откуда брать json файл
     path = 'JSON_files/%s.json' % json_name
     if default == True:
--- Tests/SESL/__init__.py
+++ Tests/SESL/__init__.py
@@ -1,3 +1,3 @@
-import os
+import os, sys
 
-os.chdir(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, os.path.dirname(__file__))
