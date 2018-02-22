17b99ffc24582e24f9ec3bb8412e41a42cc019ee
Viktor <Viktor.Kliui@gmail.com>
2018-02-22 19:03:04 +0200

    refactored structure

--- bin/Session.py
+++ bin/Session.py
@@ -1,11 +1,16 @@
 import requests, json
 
 
-class Client(object):
-    def __init__(self):
-        self.session = requests.Session
-        self.session.headers = {'content-type': "application/json;charset=UTF-8"}
-        self.auth = ("Root", "Smidle098adm!")
+class Client:
+    session = requests.Session
+    session.headers = {'content-type': "application/json;charset=UTF-8"}
+    session.auth = ("Root", "Smidle098adm!")
 
-    def get(self, req_url, req_params):
-        return self.session.get(url=req_url, params=req_params)
+    def get(req_url, req_params):
+        return session.get(url=req_url, params=req_params)
+
+
+
+
+
+print(Client.get(req_url="http://172.22.2.66:8080/SmiddleQualityService/qos/report/metadata", req_params={}))
